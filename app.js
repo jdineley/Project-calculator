55374c6421d04e1555e04e950e04c8a09583e2ca
James Dineley <jdineley@hotmail.com>
2022-09-11 22:43:04 +0100

    Middle of debugging

--- app.js
+++ app.js
@@ -1,6 +1,46 @@
 // Bugs 
 // 1. can't have two decimal points in one calc (2.2+4.4)
 // 2. likes 8*.2  dislikes .2*8  Problem with validate let regex = /\d+[+|\-|*|\/]\d+/;
+// 3. delete screws with button deactiveation logic allowing 2.1.
+//    solution will be to write a better activate/deactivate logic
+//    a. with ANS in the array
+//    b. without ANS in the array
+// improve equal function by:
+// string.split('*') to remove operator
+// log which operator was removed
+// use arr.reduce on the remaining array to return the result: return acc * curVal
+
+
+// General Calculator functionality:
+// Display1 = top section highlighting the current calculation (LH aligned), which is stored in variable inputStr
+// Display2 = bottom section shows result (RH aligned)
+// 1. Click clear (calls clear()) (or restarting the appication) does the following:
+    //  Activates all numButs
+    //    1a Click a numBut does
+    //          updates inputStr which simultaneously updates Display1
+    //          Checks inputStr against valid calculation string
+    //              if(true){equalsBut active}
+    //                1b Click equalBut does
+    //                      use inputStr to call the appropriate operation function
+    //                      write the result of operation function into Display2
+    //                      make inputStr = result of operation
+    //                      deactivate equalBut
+    //                      deactivate opBut
+    //                      deactivate deleteBut
+    //          Activates delBut & opBut
+    //           1c Click opBut does:
+    //                  updates inputStr which simultaneously updates Display1
+    //           1d Click delBut does:
+    //                  removes last character from inputStr which simultaneously updates Display1
+    //  Deactivates equals, operation and delete buttons
+// 2. Click numBut does
+//      calls clear()... return to #1
+// 3. Click opBut
+//      write ANS into Display1      
+//      
+//    |-----  2
+// 1 -|-----  3
+//    |-----  4
 
 const display1 = document.querySelector(".display1");
 const display2 = document.querySelector(".display2");
@@ -12,20 +52,18 @@ const decPointBut = document.querySelector(".point");
 const delBut = document.querySelector(".del");
 
 const numberRegex = /\d/;
-const opRegex = /[+|\-|*|\/]/g;
+const opRegex = /[\+\-\*\/]/;
 const decPointRegex = /\./g;
 const validArrayCalcRegex = /\d+[+|\-|*|\/]\d+/;
 
 
 let inputArr = [];
 let calcStarted = false;
+let button;
 
 function validate(regex, str){
     return regex.test(str);
 }
-
-// There is repeated functionality throughout my program
-// I need to modularise the repeated operations into named functions and reuse them 
 
 function activatebuttons(button){
     switch(button) {
@@ -84,44 +122,56 @@ function deactivatebuttons(button){
 
 
 function calculation(e) {
+    console.log(e.target.textContent, calcStarted)
     activatebuttons(equalsBut);
     activatebuttons(delBut);
+    activatebuttons(decPointBut);
     if(!calcStarted){
         button = e.target.textContent;
         let regex3 = /\d/;
-        if(display1.textContent === 'ANS' && regex3.test(button) ) {
+        let regex4 = /\./;
+        if(display1.textContent === 'ANS' && regex3.test(button)) {
             return
         }
         
         let clearButs = ["DEL", "C"];
         if(!clearButs.includes(button)) {
-            console.log('yosss')
             display1.textContent += button;
             // Quick way to check if a number button is pressed
             if(+button){
                 inputArr.push(+button);
-            } 
+            }  
             else {
                 inputArr.push(button);
             }
             // Make operator buttons inactive after one is used
-            let regex1 = /[+|\-|*|\/]/g
+            let regex1 = /[\+\-\*\/]/g
             let regex2 = /\./g
+            let regex3 = /[\+\-\*\/\.]/g
             let opMatch = display1.textContent.match(regex1);
             let pointMatch = display1.textContent.match(regex2);
+            let combMatch = display1.textContent.match(regex3);
+            console.log(display1.textContent.split(regex3));
             if (opMatch && opMatch.length === 1){
                deactivatebuttons(opButs)
             }
             if (pointMatch && pointMatch.length === 1){
+
                deactivatebuttons(decPointBut);
             }
+            if (combMatch && combMatch.length === 2 && !(display1.textContent.split(regex3).includes('ANS')) ){
+                activatebuttons(decPointBut);
+            }
+            if (combMatch && combMatch.length === 3){
+                deactivatebuttons(decPointBut);
+            }
+            // if (combMatch && combMatch[combMatch.length-1] === '.'){
+            //     deactivatebuttons(decPointBut);
+            // }
         }
     } else {
         // after equals, operate on ANS or write new number and start again
         button = e.target.textContent;
-        // let regex1 = /\d/;
-        // let regex2 = /[+|\-|*|\/]/g;
-        // let regex3 = /\./g
         if(validate(numberRegex, button)){
             inputArr = [button]
             display1.textContent = button;
@@ -144,13 +194,11 @@ function equals(e) {
    deactivatebuttons(delBut);
    activatebuttons(numButs);
    activatebuttons(opButs);
-   activatebuttons(decPointBut);
+   deactivatebuttons(decPointBut);
     calcStarted = true;
     let inputStr = inputArr.join('');
-    console.log(inputStr)
     let regex = /\d+[+|\-|*|\/]\d+/;
     let valid = regex.test(inputStr);
-    console.log(valid)
     if(valid){
         if(inputArr.includes('+')){
             let firstNum = inputArr.slice(0, ((inputArr.indexOf('+')))).join('')
@@ -203,29 +251,10 @@ function del(){
 }
 
 
-// function plusMinusToggle(){
-//     if()
-// }
-
-
-
-// numButs.forEach((but) => {
-//     but.addEventListener('click', calculation);
-// })
 activatebuttons(numButs);
 activatebuttons(opButs);
 activatebuttons(decPointBut);
 activatebuttons(clearBut);
-
-// opButs.forEach((but) => {
-//     but.addEventListener('click', calculation);
-// })
-
-// decPointBut.addEventListener('click', calculation);
-
-
-// clearBut.addEventListener('click', clear);
-
 
 
 
